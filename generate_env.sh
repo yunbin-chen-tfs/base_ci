b79d84ed5c0b3b239f6ab12382831192b69b88ed
klhopital <kevin.lhopital@savoirfairelinux.com>
2021-08-03 16:17:19 +0200

    docker: add tftp-dir and nfs-dir options
    In order to be able to use nfs or/and tftp server on host instead of
    docker ones, add tftp-dir and nfs-dir options in generate_env script.
    These options are here to get the path were host servers are configured.
    If these options are not set, they are set to the default path in
    jenkins_home.
    
    Issue: #6076
    Change-Id: I19daa998002b9f047cbad99f4715ef24a8b93f98

--- generate_env.sh
+++ generate_env.sh
@@ -2,10 +2,41 @@
 
 source_dir=$(dirname "$0")
 
-if [ $# -gt 0 ] ; then
-    echo "Error this script takes no arguments"
-    exit 1
-fi
+usage()
+{
+    echo 'Generate jenkins environment'
+    echo 'usage: ./generate_env.sh [-h] [options]'
+    echo
+    echo 'optional arguments:'
+    echo '  --tftp-dir TFTP_DIR set     tftp dir when using host tftp server'
+    echo '  --nfs-dir NFS_DIR set       nfs dir when using host nfs server'
+    echo '  -h, --help                  show this help message and exit'
+}
+
+options=$(getopt -o hi:d: --long tftp-dir:,nfs-dir:,help -- "$@")
+
+eval set -- "$options"
+while true; do
+    case "$1" in
+    -h|--help)
+        usage
+        exit 0
+        ;;
+    --tftp-dir)
+        shift
+        tftp_dir="$1"
+        ;;
+    --nfs-dir)
+        shift
+        nfs_dir="$1"
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
+done
 
 cd "${source_dir}"
 
@@ -18,16 +49,22 @@ if [ ! -d /var/jenkins_home ] ; then
     sudo chown 1000:1000 /var/jenkins_home
 fi
 
-# Create TFTP shared folder
-if [ ! -d /var/jenkins_home/tftp ] ; then
-    sudo mkdir -p -v /var/jenkins_home/tftp
-    sudo chown 1000:1000 /var/jenkins_home/tftp
+# Create TFTP shared folder if tftp_dir is not set
+if [ -z "${tftp_dir}" ] ; then
+    tftp_dir="/var/jenkins_home/tftp"
+    if [ ! -d /var/jenkins_home/tftp ] ; then
+        sudo mkdir -p -v /var/jenkins_home/tftp
+        sudo chown 1000:1000 /var/jenkins_home/tftp
+    fi
 fi
 
-# Create NFS shared folder
-if [ ! -d /var/jenkins_home/nfs ] ; then
-    sudo mkdir -p -v /var/jenkins_home/nfs
-    sudo chown 1000:1000 /var/jenkins_home/nfs
+# Create NFS shared folder if nfs_dir is not set
+if [ -z "${nfs_dir}" ] ; then
+    nfs_dir="/var/jenkins_home/nfs"
+    if [ ! -d /var/jenkins_home/nfs ] ; then
+        sudo mkdir -p -v /var/jenkins_home/nfs
+        sudo chown 1000:1000 /var/jenkins_home/nfs
+    fi
 fi
 
 # For using yocto cache
@@ -40,5 +77,7 @@ timezone=$(timedatectl status | awk '/Time zone/{ print $3 }')
 
 cat >.env << EOF
 _CI_DOCKER_GID=${docker_gid}
+NFS_DIR=${nfs_dir}
+TFTP_DIR=${tftp_dir}
 TIMEZONE=${timezone}
 EOF
